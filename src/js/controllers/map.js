e4ee36ae2b2ad066b34f00fea46c0486fd509a34
Dzmitry Kryvou <5625210@gmail.com>
2018-02-09 01:38:29 +0300

    Without geolocation & voice

---
+++ src/js/controllers/map.js
@@ -0,0 +1,9 @@
+/* setTimer = setTimeout(function(){
+ console.log("123333");
+ // gameService.won = "Поздравляем!!! Вы победили!";
+ /!*  $scope.hide();
+ window.location.href = '#/tables'*!/}, 1000);*/
+// var setTimer;
+// clearTimeout(setTimer)
+// window.location.reload();
+// window.location.href = '#/'
